cfb1da2b332d84031cb088d96fc4d80c39fed45f
nitin-jaswal24 <94535054+nitin-jaswal24@users.noreply.github.com>
2024-03-20 16:46:16 +0530

    Update Register.jsx

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -53,7 +53,7 @@ const Register = () => {
       <section className="authPage">
         <div className="container">
           <div className="header">
-            <img src="../../../public/images/logofinal.png" alt="logo" />
+            <img src="/images/logofinal.png" alt="logo" />
             <h3>Create a new account</h3>
           </div>
           <form>
